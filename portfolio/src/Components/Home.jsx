f6d73455b4d22fbdd657ef7a415b11bb995eb532
Gabinlavin <dcgabriel2017@gmail.com>
2023-11-14 11:50:37 -0300

    primeros pasos

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import developer from "../assets/developer.png";
+import header from "../assets/header.png";
 
 const Home = () => {
   return (
-    <div className='h-screen w-screen overflow-hidden'>
-        
+    <div className=' h-screen w-screen overflow-hidden'>
+        <img alt='header' src={header} className='w-screen' ></img>
+        <img
+                alt='developer'
+                src={developer}
+                className='absolute left-[50%] top-[130px]'
+                />
+                <div className="absolute left-[20%] top-[40%] w-[619px] h-[250px] text-black text-[64px] font-normal">
+                    <p> ¡Hola! Soy Gabriel Full Stack Developer </p>
+                </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      "2xl": { max: "2000px" },
+      // => @media (max-width: 1535px) { ... }
+    },
   },
   plugins: [],
-}
-
+};
